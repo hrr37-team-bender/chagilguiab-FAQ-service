15f6691f2fae4526964257a936874e1eb5adf395
Chagil Guiab <chagilguiab@gmail.com>
2019-04-11 23:47:58 -0700

    styles Features component

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from '../css-modules/Features.css';
 
 const Features = (props) => (
-    <ul>
+  <div className="features-div">
+    <ul className="features-ul">
       {props.features.map((feature, idx) => {
         return <li key={idx}>{feature}</li>
       })}
     </ul>
+  </div>
 )
 
 export default Features;
