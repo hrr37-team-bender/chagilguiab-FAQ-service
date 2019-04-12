ddfbe5b2f39b16fd77b79461e49181237d7e8251
Chagil Guiab <chagilguiab@gmail.com>
2019-04-11 20:10:15 -0700

    adds parent FeaturesSpecs component to index.jsx that conditionally renders Faq component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Faq from './components/Faq.jsx';
 
-ReactDOM.render(<Faq />, document.getElementById('Faq') || document.createElement('div'));
+class FeaturesSpecs extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      listOpen: false
+    }
+
+    this.setState = this.setState.bind(this);
+    this.toggleList = this.toggleList.bind(this);
+  }
+      toggleList () {
+        this.setState({listOpen: !this.state.listOpen});
+
+  }
+  render () {
+    return (
+      <div onClick={()=> this.toggleList()}>
+        <h3>Features and Specs</h3>
+        {this.state.listOpen
+          ? <Faq />
+          : null
+        }
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<FeaturesSpecs/>, document.getElementById('FeaturesSpecs') || document.createElement('div'));
