f57e3c13390715f8d244f0d0a1194b185927fc20
Chagil Guiab <chagilguiab@gmail.com>
2019-04-11 17:28:16 -0700

    defines schema

---
+++ schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS deepfryd_faq;
+
+CREATE DATABASE deepfryd_faq;
+
+USE deepfryd_faq;
+
+CREATE TABLE stereos (
+  id int NOT NULL AUTO_INCREMENT,
+  deepfryd_id varchar(100),
+  features varchar(100),
+  question varchar(100),
+  answer varchar(100),
+  tech_spec varchar(100),
+  PRIMARY KEY (ID)
+);
